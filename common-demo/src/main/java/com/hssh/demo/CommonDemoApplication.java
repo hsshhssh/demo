82c2ef161b96b23d9262d4dc3ede261b87c09b6b
hsshhsshhssh <346731187@qq.com>
2017-09-15 22:03:43 +0800

    fix

--- common-demo/src/main/java/com/hssh/demo/CommonDemoApplication.java
+++ common-demo/src/main/java/com/hssh/demo/CommonDemoApplication.java
@@ -1,15 +1,19 @@
 package com.hssh.demo;
 
 import com.hssh.demo.http.HttpTest;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class CommonDemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException
+	{
 		//SpringApplication.run(CommonDemoApplication.class, args);
 
-		System.out.println(HttpTest.get(args[0]));
+		//System.out.println(HttpTest.get(args[0]));
+
+		HttpTest.httpsGet("https://isdtuis.5fun.com/feedback/userbx/ltgkvT?appid=20&clickid=3586672&idfa=EAD79E12-311C-44E5-B65A-1DA31AD17ABD");
 	}
 }
--- common-demo/src/main/java/com/hssh/demo/http/HttpTest.java
+++ common-demo/src/main/java/com/hssh/demo/http/HttpTest.java
@@ -12,10 +12,9 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.protocol.HTTP;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
+import java.net.URL;
 import java.util.List;
 
 /**
@@ -115,5 +114,24 @@ public class HttpTest
         }
 
         return genHttpResult(response);
+    }
+
+    public static void httpsGet(String url) throws IOException
+    {
+        // 创建URL对象
+        URL myURL = new URL(url);
+
+        // 创建HttpsURLConnection对象，并设置其SSLSocketFactory对象
+        HttpsURLConnection httpsConn = (HttpsURLConnection) myURL.openConnection();
+
+        // 取得该连接的输入流，以读取响应内容
+        InputStreamReader insr = new InputStreamReader(httpsConn.getInputStream());
+
+        // 读取服务器的响应内容并显示
+        int respInt = insr.read();
+        while (respInt != -1) {
+            System.out.print((char) respInt);
+            respInt = insr.read();
+        }
     }
 }
